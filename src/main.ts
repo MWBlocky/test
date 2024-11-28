e75795194fff3c1b7bc0b1def16955cfff7b412c
Michał Walczak <michal.walczak@blockydevs.com>
2024-11-28 12:05:26 +0100

    Initialize app before starting server

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  await app.init();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
