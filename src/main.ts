2b72addee5d1006490481892c292698069b61869
Michał Walczak <michal.walczak@blockydevs.com>
2024-11-28 12:37:24 +0100

    Add static Swagger UI hosting with Express integration
    Replaced dynamic Swagger document creation with a static setup leveraging Express to serve Swagger UI files. This enhances performance by serving pre-generated static files and aligns with the new route rewrites introduced in vercel.json, ensuring consistent API access through '/api' endpoints.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as path from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,10 +13,19 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('cats')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
 
-  await app.init();
+  const swaggerUiDistPath = path.join(
+    __dirname,
+    '..',
+    'node_modules',
+    'swagger-ui-dist',
+  );
+  app.use('/swagger-static', express.static(swaggerUiDistPath));
+
+  // Swagger UI
+  app.use('/api', express.static(swaggerUiDistPath, { index: false }));
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
