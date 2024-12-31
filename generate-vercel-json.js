cbe79ac3177672bdc44c2600021b4eb5042eec9b
Michał Walczak <michal.walczak@blockydevs.com>
2024-12-31 16:05:28 +0100

    Standardize quote usage and remove unnecessary code.
    Replaced double quotes with single quotes for consistency in the Vercel configuration. Removed redundant error handling block during the `vercel.json` file write operation to simplify code.

--- generate-vercel-json.js
+++ generate-vercel-json.js
@@ -1,29 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
 
 const vercelConfig = {
   version: 2,
   builds: [
     {
-      src: "src/main.ts",
-      use: "@vercel/node",
+      src: 'src/main.ts',
+      use: '@vercel/node',
     },
   ],
   routes: [
     {
-      src: "/(.*)",
-      dest: "src/main.ts",
+      src: '/(.*)',
+      dest: 'src/main.ts',
       headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "X-Requested-With,Content-Type,Accept",
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type,Accept',
       },
     },
   ],
 };
 
-fs.writeFileSync(
-  './vercel.json',
-  JSON.stringify(vercelConfig), (err) => {
-    if (err) throw err;
-    console.log('vercel.json written');
-  });
+fs.writeFileSync('./vercel.json', JSON.stringify(vercelConfig));
