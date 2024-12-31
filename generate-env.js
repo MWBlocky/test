e3022654b90d2b86ed5f15de708a7cae0071773f
Michał Walczak <michal.walczak@blockydevs.com>
2024-12-31 16:28:10 +0100

    Generate vercel.json dynamically in generate-env.js
    Moved the vercel.json configuration into generate-env.js for dynamic generation. This consolidates configuration management and eliminates the need for a static vercel.json file.

--- generate-env.js
+++ generate-env.js
@@ -1,12 +1,35 @@
 const fs = require('fs');
+const path = require('path');
 
-const testEnv = "PRODUCTION=true";
-
-// Zapisanie zmiennej do pliku .env
-fs.writeFileSync('./.env', testEnv, 'utf8', (err) => {
-  if (err) {
-    console.error("Błąd podczas zapisywania pliku .env:", err);
-  } else {
-    console.log('.env został utworzony');
-  }
-});
+const vercelConfig = {
+  version: 2,
+  builds: [
+    {
+      src: 'src/main.ts',
+      use: '@vercel/node',
+    },
+  ],
+  routes: [
+    {
+      src: '/(.*)',
+      dest: 'src/main.ts',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type,Accept',
+      },
+    },
+  ],
+};
+fs.writeFileSync(
+  path.join(process.cwd(), 'vercel.json'),
+  JSON.stringify(vercelConfig, null, 2),
+  'utf8',
+  (err) => {
+    if (err) {
+      console.error('Błąd podczas zapisywania pliku vercel.json:', err);
+    } else {
+      console.log('Plik vercel.json został wygenerowany.');
+    }
+  },
+);
