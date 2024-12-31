76b638d20d1060d5af2f310e387460e5ad494333
Michał Walczak <michal.walczak@blockydevs.com>
2024-12-31 15:47:44 +0100

    Refactor Vercel config structure in generate-vercel-json.js
    Simplified the configuration by reordering and separating `builds` and `routes` arrays for clarity. This change improves readability and maintains the same functional behavior.

--- generate-vercel-json.js
+++ generate-vercel-json.js
@@ -2,27 +2,22 @@ const fs = require('fs');
 
 const vercelConfig = {
   version: 2,
-  rewrites: [
+  builds: [
     {
-      "version": 2,
-      "builds": [
-        {
-          "src": "src/main.ts",
-          "use": "@vercel/node"
-        }
-      ],
-      "routes": [
-        {
-          "src": "/(.*)",
-          "dest": "src/main.ts",
-          "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Headers": "X-Requested-With,Content-Type,Accept"
-          }
-        }
-      ]
-    }
+      src: "src/main.ts",
+      use: "@vercel/node",
+    },
+  ],
+  routes: [
+    {
+      src: "/(.*)",
+      dest: "src/main.ts",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "X-Requested-With,Content-Type,Accept",
+      },
+    },
   ],
 };
 
